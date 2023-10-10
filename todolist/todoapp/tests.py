74030bb4612e92a41f1236b4885effce3656a686
chi <m23568n@gmail.com>
2023-10-10 18:42:37 +0800

    新增測試拿取全部todos和新增新的todo

--- todolist/todoapp/tests.py
+++ todolist/todoapp/tests.py
@@ -20,10 +20,8 @@ data = [
 class ToDoListTestCase(TestCase):
     def setUp(self) -> None:
         # 新增資料
-        todo1 = Todo(title=data[0]['title'], complete=data[0]['complete'])
-        todo1.save()
-        todo2 = Todo(title=data[1]['title'], complete=data[1]['complete'])
-        todo2.save()
+        Todo.objects.create(title=data[0]['title'], complete=data[0]['complete'])
+        Todo.objects.create(title=data[1]['title'], complete=data[1]['complete'])
         
         # 建立client
         self.client = Client()
@@ -35,8 +33,22 @@ class ToDoListTestCase(TestCase):
         self.assertTemplateUsed("index.html")
 
 
-    # def test_get_all_data(self):
-    #     res = self.client.get("/api/")
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(json.loads(res.content)['data'][0]['title'], data[0]['title'])
-        
+    def test_get_all_todos(self):
+        res = self.client.get("/api/")
+        res_datas = json.loads(res.content)['data']
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res_datas[0]['title'], data[0]['title'])
+
+
+    def test_add_new_todo(self):
+        test_data = {'title': 'test 003'}
+        res = self.client.post(
+                "/api/add/", 
+                json.dumps(test_data), 
+                content_type="application/json"
+            )
+        res_data = json.loads(res.content)
+        self.assertEqual(res_data['todo_title'], test_data['title'])
+        self.assertEqual(res_data['complete'], False)
+    
+    
